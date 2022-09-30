aed61d7eb5829f02a68f2d6bae022b7751d37010
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-30 00:44:00 -0300

    feat: create routes file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
-
-
+import router from "./routes";
 
 const app = express();
 
 app.use(json());
 app.use(cors());
-
+app.use(router)
 
 export default app;
