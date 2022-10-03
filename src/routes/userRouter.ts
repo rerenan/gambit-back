3d370a2761f3ad8bfd03669ebc97d09ddf84768d
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-02 23:23:31 -0300

    feat: applying schema validator into user router

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/userController";
+import schemaMiddleware from "../middlewares/schemaMiddleware";
+import { loginUserSchema, registerUserSchema } from "../utils/schemas/userSchema";
 
 const userRouter = Router();
 
-userRouter.post("/", loginUser);
-userRouter.post("/register", registerUser);
+userRouter.post("/", schemaMiddleware(loginUserSchema), loginUser);
+userRouter.post("/register", schemaMiddleware(registerUserSchema), registerUser);
 
 export default userRouter;
