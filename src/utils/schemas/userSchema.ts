7733dc1cebd7e209f277804e65a5cd522fce3f78
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-02 23:21:26 -0300

    feat: add register user schema

--- src/utils/schemas/userSchema.ts
+++ src/utils/schemas/userSchema.ts
@@ -4,3 +4,12 @@ export const loginUserSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().min(4).required(),
 });
+
+export const registerUserSchema = joi.object({
+    name: joi.string().email().required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(4).required(),
+    confirmPassword: joi.string().valid(joi.ref('password')).messages({
+        'any.only': "password and confirmPassword they're not the same"
+    })
+});
