502b245a54cb912837518df790a0a2f190b621dd
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-02 23:08:25 -0300

    feat: add find by username in user repository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -2,10 +2,11 @@ import prisma from "../config/database";
 import { UserInsertData } from "../types/User";
 
 async function insert(userData: UserInsertData) {
-    const {email, password } = userData;
+    const {username, email, password } = userData;
 
     return await prisma.user.create({
         data: {
+            username,
             email,
             password
         }
@@ -21,7 +22,17 @@ async function findByEmail(email: string) {
     return result;
 }
 
+async function findByUserName(username: string) {
+    const result = await prisma.user.findFirst({
+        where: {
+            username
+        }
+    });
+    return result;
+}
+
 export const userRepository = {
     insert,
-    findByEmail
+    findByEmail,
+    findByUserName
 }
