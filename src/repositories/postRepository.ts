1f6508d5b704e42f8e3811fa0974452a58963916
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-09 00:00:12 -0300

    feat: repository get user posts and order by descrecent id

--- src/repositories/postRepository.ts
+++ src/repositories/postRepository.ts
@@ -1,45 +1,55 @@
-import { PostInsertData } from './../types/Post';
+import { PostInsertData } from "./../types/Post";
 import prisma from "../config/database";
 
-function insert(postData:PostInsertData) {
-    const {userId, text} = postData;
-    return  prisma.post.create({
-        data:{
-            userId,
-            text
-        }
-    })
+function insert(postData: PostInsertData) {
+  const { userId, text } = postData;
+  return prisma.post.create({
+    data: {
+      userId,
+      text,
+    },
+  });
 }
 
-function get(userId:number) {
-    return prisma.post.findMany({
-        where:{
-            user: {
-                followers:{
-                    some:{
-                        userIdFollower: userId
-                    }
-                }
-            }
-        },
-        select:{
-            user:{
-                select: {
-                    id: true,
-                    username: true,
-                    profile: {
-                        select:{
-                            profilePicture: true
-                        }
-                    }
-                }
+function get(userId: number) {
+  return prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                userIdFollower: userId,
+              },
             },
-            text: true
+          },
+        },
+        {
+          userId
         }
-    })
+      ],
+    },
+    select: {
+      user: {
+        select: {
+          id: true,
+          username: true,
+          profile: {
+            select: {
+              profilePicture: true,
+            },
+          },
+        },
+      },
+      text: true,
+    },
+    orderBy:{
+        id: "desc"
+    }
+  });
 }
 
 export const postRepository = {
-    insert,
-    get
-}
+  insert,
+  get,
+};
